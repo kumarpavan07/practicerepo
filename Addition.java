61a80661d360e89e95ac938df39afd749bb29848
kumarpavan07 <144512096+kumarpavan07@users.noreply.github.com>
2026-04-17 19:46:37 +0530

    Update and rename AWS to Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 20;
+        
+        // Use the + operator to add numbers
+        int sum = first + second;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
