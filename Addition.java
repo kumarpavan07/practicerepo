55b56889683e17952537248d508a5aacb81f3158
kumarpavan07 <144512096+kumarpavan07@users.noreply.github.com>
2026-05-14 12:55:26 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition {
     public static void main(String[] args) {
         int first = 30;
-        int second = 20;
+        int second = 70;
         
         // Use the + operator to add numbers
         int sum = first + second;
